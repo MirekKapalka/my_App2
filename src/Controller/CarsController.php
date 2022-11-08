a5d454caff23041d4ddf2d1ed76b1b0bede8019b
MirekKapalka <miroslaw.kapalka90@gmail.com>
2022-11-08 11:26:35 +0100

    zmiana bazy danych na mariaDB i utworzenie carsControllera

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -2,12 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Cars;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
 
 class CarsController extends AbstractController
 {
+
+
+    #[Route('/cars/create', name: 'create_car')]
+    public function createProduct(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new Cars();
+        $product->setName('Opel Kadett');
+        $product->setYear(1991);
+        $product->setColor('Dark Blue');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->render('cars/create.html.twig', [
+            'controller_name' => 'CarsController',
+            'name' => $product->getName(),
+            'year' => $product->getYear(),
+            'color' => $product->getColor()
+        ]);
+    }
+
+
     #[Route('/cars', name: 'app_cars')]
     public function index(): Response
     {
