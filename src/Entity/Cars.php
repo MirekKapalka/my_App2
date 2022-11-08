d7d941ec92c1facaad04dab5f9011530403b5a53
MirekKapalka <miroslaw.kapalka90@gmail.com>
2022-11-08 08:14:35 +0100

    added postgres and adminer

---
+++ src/Entity/Cars.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarsRepository::class)]
+class Cars
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column]
+    private ?int $Year = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Color = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->Year;
+    }
+
+    public function setYear(int $Year): self
+    {
+        $this->Year = $Year;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->Color;
+    }
+
+    public function setColor(string $Color): self
+    {
+        $this->Color = $Color;
+
+        return $this;
+    }
+}
